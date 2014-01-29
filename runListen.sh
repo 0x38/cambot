2d44d848d9123707dd49908619cc0a3099888f59
Joe Rice <joesbigidea@gmail.com>
2014-01-29 03:52:09 +0000

    add a listener version of the bot launcher

---
+++ runListen.sh
@@ -0,0 +1,6 @@
+#Run and listen for a machine to connect to receive video
+#the machine must be listening when this is invoked
+raspivid -vf -t 999999 -o - | nc -l 5001 &
+sudo python robot.py
+jobs
+kill %1
